e3aec17ec72ec035d3a88e4c83c3127de331a512
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-06-23 09:29:46 +0300

    init

--- OnSubmitModifier/ContentView.swift
+++ OnSubmitModifier/ContentView.swift
@@ -7,13 +7,44 @@
 
 import SwiftUI
 
+@available(iOS 15.0, *)
 struct ContentView: View {
+    @State private var login = ""
+    @State private var password = ""
+    @State private var loggedIn = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                VStack {
+                    TextField("Login", text: $login)
+                        .textFieldStyle(.roundedBorder)
+                    TextField("Password", text: $password)
+                        .textFieldStyle(.roundedBorder)
+                    if loggedIn {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 8)
+                                .foregroundColor(Color.green)
+                            Text("LOGGED IN!")
+                                .font(.title)
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+                .frame(width: 300, height: 150)
+                .onSubmit {
+                    if !login.isEmpty && !password.isEmpty {
+                        loggedIn = true
+                    } else {
+                        loggedIn = false
+                    }
+                }
+            }
+        }
     }
 }
 
+@available(iOS 15.0, *)
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- OnSubmitModifier/OnSubmitModifierApp.swift
+++ OnSubmitModifier/OnSubmitModifierApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+@available(iOS 15.0, *)
 @main
 struct OnSubmitModifierApp: App {
     var body: some Scene {
