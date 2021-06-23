b1cb52784f77fdc3d12e5dc6ba4ace054748a61f
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-06-23 09:23:31 +0300

    Initial Commit

---
+++ OnSubmitModifier/OnSubmitModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnSubmitModifierApp.swift
+//  OnSubmitModifier
+//
+//  Created by ramil on 23.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct OnSubmitModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
